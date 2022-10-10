50e370e163f58592eff59286547341a16d778996
milan <milan@pop-os.localdomain>
2022-10-10 09:36:00 +0200

    Data structure updated

--- gdmg/src/main/java/com/app/gdmg/repositories/ReservationEntityRepository.java
+++ gdmg/src/main/java/com/app/gdmg/repositories/ReservationEntityRepository.java
@@ -4,6 +4,16 @@ import com.app.gdmg.entities.ReservationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public interface ReservationEntityRepository extends JpaRepository<ReservationEntity,Long> {
+
+    ReservationEntity findByDateDebut(Date date);
+    ReservationEntity findByDateFin(Date date);
+    ReservationEntity findByCode(String code);
+    ReservationEntity findByDateCreation(Date date);
+
+
+
 }
